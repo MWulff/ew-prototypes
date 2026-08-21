64eda07b60687083cdd21e8e5e2bced32e2c5a2f
prototype-kit <prototype-kit@earnwise.local>
2026-08-21 20:00:22 +0200

    Deploy 2026-08-21 20:00

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3110715593" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "118420987" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
